8e44371b22e9b8dfd7c835fd269d4fb7fee00351
BiBiOTA <yukiotataitien@gmail.com>
2021-11-16 17:10:31 +0800

    Pagination組建

---
+++ src/components/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-lg flex flex-center">
+    <q-pagination
+      v-model="pagination.current"
+      color="black"
+      :max="pagination.max"
+      :max-pages="6"
+      boundary-numbers
+    />
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Pagination',
+
+  props: {
+    initialPagination: Object,
+  },
+
+  data() {
+    return {
+      pagination: this.initialPagination,
+    }
+  },
+
+  methods: {},
+  created() {}
+})
+</script>
