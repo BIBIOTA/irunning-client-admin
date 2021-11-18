1bce04ea026a90d8e09d690f6178daa50ab06805
BiBiOTA <yukiotataitien@gmail.com>
2021-11-18 14:51:31 +0800

    檢視、編輯頁面button組建

---
+++ src/components/Btns.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pa-md flex flex-center q-gutter-sm">
+    <q-btn color="primary" label="返回" @click="() => $router.back()" />
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Btns',
+
+  props: {
+    back: Boolean,
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+  created() {}
+})
+</script>
