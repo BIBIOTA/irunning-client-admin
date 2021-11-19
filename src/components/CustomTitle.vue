18fccf71dadfa9ef670fb6273e4ed1f5cff8d677
BiBiOTA <yukiotataitien@gmail.com>
2021-11-19 17:24:25 +0800

    Custom Title組建

---
+++ src/components/CustomTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+      v-if="title"
+      class="text-h5 q-pl-md q-pt-md">
+        {{title}}
+    </div>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Title',
+
+  props: {
+    title: String,
+  },
+
+  setup () {
+    return {}
+  },
+  methods: {},
+  created() {}
+})
+</script>
