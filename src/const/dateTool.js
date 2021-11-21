53772e02f8370e69b6b5c9a0fc772381bb857869
BiBiOTA <yukiotataitien@gmail.com>
2021-11-21 21:24:08 +0800

    新增dateTool Strava跑步移動時間轉換 & moment.js

--- src/const/dateTool.js
+++ src/const/dateTool.js
@@ -1,4 +1,5 @@
-import { date } from 'quasar'
+import { date } from 'quasar';
+import moment from 'moment';
 
 export const sqlDateToFrontend = (sqlDate) => {
   if (sqlDate) {
@@ -6,4 +7,22 @@ export const sqlDateToFrontend = (sqlDate) => {
   } else {
     return null
   }
+};
+
+/* Strava跑步移動時間轉換 */
+export const timeFormat = (moving_time) => {
+  let hour = moment.utc(moving_time*1000).format('HH');
+  let min = moment.utc(moving_time*1000).format('mm');
+  let sec = moment.utc(moving_time*1000).format('ss');
+  let timeFormat = '';
+  if (parseInt(hour) > 0) {
+    timeFormat += `${hour}小時`;
+  }
+  if (parseInt(min) > 0) {
+    timeFormat += `${min}分`;
+  }
+  if (parseInt(sec) > 0) {
+    timeFormat += `${sec}秒`;
+  }
+  return timeFormat;
 };
